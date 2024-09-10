4e0fc095d280e107129155cd41670a751e075c38
Hexagon Six <hexagon6@fet.li>
2024-09-10 21:49:20 +0200

    feature(component): add Markdown.svelte

---
+++ src/lib/components/Markdown.svelte
@@ -0,0 +1,8 @@
+<script>
+  import * as marked from 'marked'
+  import { default as purify } from 'dompurify'
+
+  let { parse } = $props()
+</script>
+
+{@html purify.sanitize(marked.parse(parse))}
