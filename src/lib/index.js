35bf6fd3cbe95c0a94b6f6fd7c2dc456c2458e27
Hexagon Six <hexagon6@fet.li>
2024-09-12 18:58:09 +0200

    feature(lib): add Redirect SolidLogin & SolidStorage

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,8 @@
 // Reexport your entry components here
 import LDContent from './components/LDContent.svelte'
 import Markdown from './components/Markdown.svelte'
+import Redirect from './components/Redirect.svelte'
+import SolidLogin from './components/SolidLogin.svelte'
+import SolidStorage from './components/SolidStorage.svelte'
 
-export { LDContent, Markdown }
+export { LDContent, Markdown, Redirect, SolidLogin, SolidStorage }
