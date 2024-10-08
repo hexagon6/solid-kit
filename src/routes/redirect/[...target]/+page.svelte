35a56d1851909d9eab920f58532039f943db331e
Hexagon Six <hexagon6@fet.li>
2024-10-09 00:42:46 +0200

    fix: remove ignored statements

--- src/routes/redirect/[...target]/+page.svelte
+++ src/routes/redirect/[...target]/+page.svelte
@@ -2,9 +2,6 @@
   import Redirect from '$lib/components/Redirect.svelte'
 
   const { data } = $props()
-
-  export const csr = true
-  export const ssr = false
 </script>
 
 <Redirect redirect={data.redirect.target} from={'/redirect'} />
